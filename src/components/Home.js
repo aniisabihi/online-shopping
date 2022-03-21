1710cc6cf9f02d439f7cc9c385f949aae21ff9fe
Aniisa <aniisaaden@gmail.com>
2022-03-22 00:50:06 +0100

    Minor fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,21 +33,32 @@ function Home() {
           true &&
           data.items?.map((item) => (
             <div className="card" key={item.id}>
+              <div className="card-content">
+                <h6>
+                  <b>{item.name}</b>
+                </h6>
+              </div>
+
               <div className="card-image">
-                <b>
-                  {" "}
-                  <span className="card-content">{item.brand}</span>
-                </b>
-                <span className="btn-floating halfway-fab waves-effect waves-light red">
+                <img
+                  src="https://images.pexels.com/photos/577514/pexels-photo-577514.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                  alt="item"
+                />
+              </div>
+
+              <div className="card-content">
+                <span className="btn-small btn-floating right green">
                   <i className="material-icons">add</i>
                 </span>
               </div>
 
               <div className="card-content">
-                <p>{item.name}</p>
-                <p>
-                  <b>Price: {item.price} SEK</b>
-                </p>
+                <span>
+                  <p>{item.brand}</p>
+                  <p>
+                    <b>Price: {item.price} SEK</b>
+                  </p>
+                </span>
               </div>
             </div>
           ))}
